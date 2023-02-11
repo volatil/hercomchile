95d4d0c3eaa151be0515f7267fc644f09a810b74
Paulo Torrealba <paulo@buscalibre.com>
2023-02-11 03:01:22 -0300

    htaccess multipage

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,28 @@
+import { NavLink } from "react-router-dom";
+import logo from "../../assets/imagenes/logo.jpg";
+import { NOMBRESITIO } from "../../assets/js/CONST";
+
 function Header() {
 	return (
 		<header>
-			<h1>HERCOMCHILE</h1>
+			<div className="logo">
+				<NavLink to="/">
+					<img src={logo} alt={NOMBRESITIO} />
+				</NavLink>
+			</div>
+			<nav className="menu">
+				<ul>
+					<li>
+						<NavLink className={({ isActive }) => (isActive ? "tamoactivo" : "none")} to="/">inicio</NavLink>
+					</li>
+					<li>
+						<NavLink className={({ isActive }) => (isActive ? "tamoactivo" : "none")} to="/acerca">acerca</NavLink>
+					</li>
+					<li>
+						<NavLink className={({ isActive }) => (isActive ? "tamoactivo" : "none")} to="/contacto">contacto</NavLink>
+					</li>
+				</ul>
+			</nav>
 		</header>
 	);
 }
