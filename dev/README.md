c468a33723bffb5365e860f815196f9945de8cf0
Paulo Torrealba <contacto@volatil.cl>
2025-08-13 20:18:58 -0400

    rediseño

---
+++ dev/README.md
@@ -0,0 +1,25 @@
+# moiji.cl
+- [ ] 
+
+
+<!-- 
+https://github.com/volatil/moiji/actions
+
+-> C:\Windows\System32\drivers\etc\HOSTS
+127.0.0.1   moiji.local.cl
+
+C:\xampp\apache\conf\extra\httpd-xampp.conf
+<VirtualHost *:80>
+	DocumentRoot "C:\xampp\htdocs\moiji"
+	ServerName moiji.local.com
+	<Directory "C:\xampp\htdocs\moiji">
+	</Directory>
+</VirtualHost>
+
+en "C:\xampp\apache\conf\httpd.conf" agrega
+<Directory "D:/Proyectos/moiji">
+    Options Indexes FollowSymLinks
+    AllowOverride All
+    Require all granted
+</Directory>
+-->
---
+++ dev/index.php
@@ -0,0 +1,21 @@
+<?php $cerresiete = "SIUUUUUUU !!"; ?>
+
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+
+        <title>PHP</title>
+
+    </head>
+    <body>
+
+        <p>Funciono? <?php echo $cerresiete; ?></p>
+
+    </body>
+</html>
